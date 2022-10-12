b5bf771835ff101d5997736d3bfcd7a3eeb47f1e
DoniBoye <72019275+DoniBoye@users.noreply.github.com>
2022-10-12 16:37:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify_Casus {
+    internal class Client : iPlayable  {
+        static void Main(string[] args) {
+          
+        }
+        public int CurrentTime { get; set; }
+        public bool Playing { get; set; }
+        public bool Shuffle { get; set; }
+        public bool repeat { get; set; }
+
+
+    }
+}
---
+++ iPlayable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify_Casus {
+    public class iPlayable {
+        private int x;
+    }
+}
